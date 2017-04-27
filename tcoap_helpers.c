41a63053faa8f971e8632511dee1041690ba6a42
maslyakov <maslyakov@big-3.ru>
2017-04-27 21:35:37 +0300

    fixed helpers

--- tcoap_helpers.c
+++ tcoap_helpers.c
@@ -10,6 +10,148 @@
 #include "tcoap_helpers.h"
 
 
+/**
+ * @brief See description in the header file.
+ *
+ */
+uint16_t tcoap_decode_szx_to_size(const uint8_t szx)
+{
+    switch (szx)
+    {
+        case 0:
+            return TCOPA_BLOCK_SZX_VAL_0;
+
+        case 1:
+            return TCOPA_BLOCK_SZX_VAL_1;
+
+        case 2:
+            return TCOPA_BLOCK_SZX_VAL_2;
+
+        case 3:
+            return TCOPA_BLOCK_SZX_VAL_3;
+
+        case 4:
+            return TCOPA_BLOCK_SZX_VAL_4;
+
+        case 5:
+            return TCOPA_BLOCK_SZX_VAL_5;
+
+        case 6:
+            return TCOPA_BLOCK_SZX_VAL_6;
+
+        default:
+            return TCOPA_BLOCK_SZX_VAL_7;
+    }
+
+}
+
+
+/**
+ * @brief See description in the header file.
+ *
+ */
+void tcoap_fill_block2_opt(tcoap_option_data * const option, const tcoap_blockwise_data * const bw, uint8_t * const value)
+{
+
+/*
+ * Block Option Value
+ *
+ *  0
+ *  0 1 2 3 4 5 6 7
+ *  +-+-+-+-+-+-+-+-+
+ *  |  NUM  |M| SZX |
+ *  +-+-+-+-+-+-+-+-+
+ *
+ *  0                   1
+ *  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5
+ *  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  |          NUM          |M| SZX |
+ *  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *
+ *  0                   1                   2
+ *  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3
+ *  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  |                   NUM                 |M| SZX |
+ *  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *
+ */
+
+    option->num = TCOAP_BLOCK2_OPT;
+    option->value = value;
+    option->len = 1;
+    option->next = NULL;
+
+    value[0] = (bw->number.num8[0] & 0x0F);
+    value[0] <<= 4;
+    value[0] |= bw->opt.block_szx;
+    value[0] |= bw->opt.more ? 8 : 0;
+
+    if (bw->number.num32 > 15) {
+        option->len = 2;
+
+        value[1] = value[0];
+
+        value[0] = (bw->number.num8[0] >> 4);
+        value[0] |= (bw->number.num8[1] & 0x0F);
+    }
+
+    if (bw->number.num32 > 4095) {
+        option->len = 3;
+
+        value[2] = value[1];
+        value[1] = value[0];
+
+        value[0] = (bw->number.num8[1] >> 4);
+        value[0] |= (bw->number.num8[2] & 0x0F);
+    }
+}
+
+
+/**
+ * @brief See description in the header file.
+ *
+ */
+void tcoap_extract_block2_from_opt(const tcoap_option_data * const block2, tcoap_blockwise_data * const bw)
+{
+    switch (block2->len) {
+
+        case 0:
+            bw->number.num32 = 0;
+            bw->opt.block_szx = 0;
+            bw->opt.more = 0;
+            break;
+
+        case 1:
+            bw->number.num32 = (block2->value[0] >> 4);
+            bw->opt.block_szx = (block2->value[0] & 7);
+            bw->opt.more = (block2->value[0] & 8);
+            break;
+
+        case 2:
+            bw->number.num32 = block2->value[0];
+            bw->number.num32 <<= 8;
+            bw->number.num32 |= (block2->value[1] >> 4);
+
+            bw->opt.block_szx = (block2->value[1] & 7);
+            bw->opt.more = (block2->value[1] & 8);
+            break;
+
+        case 3:
+            bw->number.num32 = block2->value[0];
+            bw->number.num32 <<= 8;
+            bw->number.num32 |= block2->value[1];
+            bw->number.num32 <<= 8;
+            bw->number.num32 |= (block2->value[2] >> 4);
+
+            bw->opt.block_szx = (block2->value[2] & 7);
+            bw->opt.more = (block2->value[2] & 8);
+            break;
+
+        default:
+            break;
+    }
+}
+
 
 /**
  * @brief See description in the header file.
@@ -34,35 +176,4 @@ const tcoap_option_data * tcoap_find_option_by_number(const tcoap_option_data * 
     return NULL;
 }
 
-
-/**
- * @brief See description in the header file.
- *
- */
-void tcoap_extract_int_value(const uint8_t * const src, uint8_t * const dst, const uint32_t len)
-{
-    uint32_t idx = 0;
-
-    switch(len) {
-
-        case 4:
-            dst[idx++] = src[3];
-            //no break
-
-        case 3:
-            dst[idx++] = src[2];
-            //no break
-
-        case 2:
-            dst[idx++] = src[1];
-            //no break
-
-        case 1:
-            dst[idx] = src[0];
-            //no break
-
-        default:
-            break;
-    }
-}
 
--- tcoap_helpers.h
+++ tcoap_helpers.h
@@ -15,6 +15,79 @@
 #include "tcoap.h"
 
 
+typedef enum {
+
+    TCOPA_BLOCK_SZX_VAL_0       = 16,
+    TCOPA_BLOCK_SZX_VAL_1       = 32,
+    TCOPA_BLOCK_SZX_VAL_2       = 64,
+    TCOPA_BLOCK_SZX_VAL_3       = 128,
+    TCOPA_BLOCK_SZX_VAL_4       = 256,
+    TCOPA_BLOCK_SZX_VAL_5       = 512,
+    TCOPA_BLOCK_SZX_VAL_6       = 1024,
+
+    /**
+     * Reserved, i.e., MUST NOT be sent and
+     * MUST lead to a 4.00 Bad Request response
+     * code upon reception in a request.
+     */
+    TCOPA_BLOCK_SZX_VAL_7       = 0
+
+} tcoap_blockwise_szx;
+
+
+typedef struct tcoap_blockwise_data {
+
+    struct {
+        /**
+        * size of block = 2 ^ (szx + 4)
+        * szx from 0 to 6
+        * szx 7 is reserved (should not use it)
+        */
+        uint8_t block_szx   : 3;
+
+        uint8_t more        : 1;         /* more (not last block) */
+    } opt;
+
+    union {
+        uint8_t num8[4];
+        uint32_t num32;
+    } number;                            /* number of block, from 0 */
+
+} tcoap_blockwise_data;
+
+
+
+/**
+ * @brief Get block size by SZX value
+ *
+ * @param szx - a three-bit unsigned integer indicating the size of a block to the power of two.
+ *
+ */
+uint16_t tcoap_decode_szx_to_size(const uint8_t szx);
+
+
+/**
+ * @brief Fill block2 option
+ *
+ * @param option - pointer on the 'tcoap_option_data' struct
+ * @param bw - pointer on the block2 data
+ * @param value - buffer for storing encoded value (max length is 3)
+ *
+ */
+void tcoap_fill_block2_opt(tcoap_option_data * const option, const tcoap_blockwise_data * const bw, uint8_t * const value);
+
+
+/**
+ * @brief Extract block2 value from option
+ *
+ * @param block2 - raw option contains block2
+ * @param bw - pointer on the block2 data to store value
+ *
+ */
+void tcoap_extract_block2_from_opt(const tcoap_option_data * const block2, tcoap_blockwise_data * const bw);
+
+
+
 /**
  * @brief Find an option by its number
  *
@@ -24,17 +97,6 @@
  * @return pointer to the found option or NULL if option is absent
  */
 const tcoap_option_data * tcoap_find_option_by_number(const tcoap_option_data * options, const uint16_t opt_num);
-
-
-/**
- * @brief Extract an integer value from the buffer with network byte order
- *
- * @param src - pointer on the src buffer
- * @param dst - pointer on the dst buffer
- * @param len - length of data, from 1 till 4
- *
- */
-void tcoap_extract_int_value(const uint8_t * const src, uint8_t * const dst, const uint32_t len);
 
 
 
